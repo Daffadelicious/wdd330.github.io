23f7415832dca9940c98450b20ff6ca63d250a53
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2021-09-18 00:41:09 -0700

    added js for toc

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Create an array that holds info for table of constants
+const links = [
+    ["Week 01 Notes", "week1/index.html"],
+    ["Week 02 Notes", "#"]
+  ]
+
+// Create function that uses links array to create table of contants
+function createTableOfContents(links){
+
+    // Access ol on index.html
+    var toc = document.getElementById("toc");
+
+    // Loop for every item in links
+    for (const link of links){
+        // Create an li and a tag
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        
+        // Add title, href, and text to a tag
+        a.title = link[0];
+        a.href = link[1];
+        var atext = document.createTextNode(link[0]);
+        a.appendChild(atext);
+
+        // Add li to ol
+        toc.appendChild(li);
+        // Add a to li
+        li.appendChild(a);    
+    }
+}
+
+// Call TOC function
+createTableOfContents(links);
